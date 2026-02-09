39eaf9f695b44bb8151192f761a2a035f00e9556
zyhwan <145274494+zyhwan@users.noreply.github.com>
2026-02-09 22:48:25 +0900

    mutable 사용

---
+++ C++/C++/mutable 사용.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class A {
+    mutable int data_;
+
+public:
+    A(int data) : data_(data) {}
+    void DoSomething(int x) const {
+        data_ = x;  // °¡´É!
+    }
+
+    void PrintData() const { std::cout << "data: " << data_ << std::endl; }
+};
+
+int main() {
+    A a(10);
+    a.DoSomething(3);
+    a.PrintData();
+}
