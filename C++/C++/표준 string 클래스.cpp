0ff3bf1b771eb1d20cbbb757440e143c575a50bd
zyhwan <145274494+zyhwan@users.noreply.github.com>
2026-02-11 16:09:35 +0900

    Update 표준 string 클래스.cpp

--- C++/C++/표준 string 클래스.cpp
+++ C++/C++/표준 string 클래스.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -7,8 +8,18 @@ int main() {
 
 	// 표준이므로 std 안에string 이 정의되어 있습니다.
 	string s = "abc";
+	string t = "def";
+	string s2 = s;
 
-	cout << s << endl;
+	cout << s << "의 길이 : " << s.length() << endl;
+	cout << s << "뒤에 " << t << "를 붙이면 : " << s + t << endl;
+
+	if (s == s2) {
+		cout << s << "와 " << s2 << "는 같다." << endl;
+	}
+	if (s != t) {
+		cout << s << "와 " << t << "는 다르다." << endl;
+	}
 
 	return 0;
 }
