a44ff9f238982772b6b160d6180c921306573f4a
zyhwan <145274494+zyhwan@users.noreply.github.com>
2026-02-14 11:03:55 +0900

    입출력 예제

---
+++ C++/C++/씹어먹는C++/입출력 라이브러리/입출력 예제.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+
+int main() {
+
+	int num{};
+
+	while (cin >> num) {
+		cout << "ют╥б : " << num << endl;
+		if (num == 0) break;
+	}
+
+
+}
