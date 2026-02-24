bfff57e9845c3f5c200656e6483f66af357c7adf
zyhwan <zyhwan010410@naver.com>
2026-02-24 17:43:06 +0900

    Update 임의의 개수의 문자열을 합치는 함수.cpp

--- C++/C++/씹어먹는C++/템플릿/임의의 개수의 문자열을 합치는 함수.cpp
+++ C++/C++/씹어먹는C++/템플릿/임의의 개수의 문자열을 합치는 함수.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
+#include<string>
 
 using namespace std;
 
+template <typename String>
+string StrCat(const String& s)
+{
+	return string(s);
+}
+
+template<typename String, typename...Strings>
+string StrCat(const String& s, Strings...strs)
+{
+	return string(s) + StrCat(strs...);
+}
+
+
+
 int main() 
 {
-
+	cout << StrCat(string("this"), " ", "is", " ", string("a"),
+		" ", string("sentence")) << endl;
+	return 0;
 }
